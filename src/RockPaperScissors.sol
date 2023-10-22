4971b88c1237ac0fd54be9c714db916e9a2f3250
zxriptor <zxriptor@gmail.com>
2023-10-22 21:33:23 +0300

    unused import removed

--- src/RockPaperScissors.sol
+++ src/RockPaperScissors.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.20;
 
 import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
-import "openzeppelin-contracts/contracts/utils/Address.sol";
 
 /// @notice These are all options a player can bet on.
 enum Bet {
